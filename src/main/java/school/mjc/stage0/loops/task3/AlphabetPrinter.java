3966ccb36edea83affc89651c7a77da7de53ef90
MirrorwindAbyss <abdumalikunusov06@gmail.com>
2023-07-31 10:01:49 +0500

    Done

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -3,5 +3,6 @@ package school.mjc.stage0.loops.task3;
 public class AlphabetPrinter {
     public void printAlphabet() {
         System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
